389c907c1fdff8dfa94454b70b2ecbf1a305d611
Albert Oliva <oliva.albert+1@gmail.com>
2025-09-13 11:20:35 +0200

    add delete app test

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -36,10 +36,35 @@ test.describe('add an app to home page favourites apps', () => {
     apps = new AppsPage(page);
     let appAdded = await apps.addRandomApp(favoriteAppsList);
 
-    // Get the updated list of favorite apps
+    // Get the updated list of favorite apps and verify the app has been added and no other app
     let updatedFavoriteAppsList = await homePage.getAppList();
     expect(updatedFavoriteAppsList.length).toBe(favoriteAppsList.length + 1);
     expect(updatedFavoriteAppsList).toContain(appAdded);
+  });
+});
+
+test.describe('delete apps in the home page favourite apps row', () => {
+  test('delete an app', async ({ page }) => {
+    homePage = new HomePage(page);
+    let favoriteAppsList = await homePage.getAppList();
+
+    if (favoriteAppsList.length === 0) {
+      // If there are no favorite apps, add one first
+      await homePage.goToTab('Apps');
+      apps = new AppsPage(page);
+      let appAdded = await apps.addRandomApp(favoriteAppsList);
+      favoriteAppsList = await homePage.getAppList();
+      expect(favoriteAppsList).toContain(appAdded);
+    }
+
+    // Now delete the first app in the favorite apps list
+    const appToDelete = favoriteAppsList[0];
+    await homePage.deleteApp();
+
+    // Verify the app has been deleted
+    let updatedFavoriteAppsList = await homePage.getAppList();
+    expect(updatedFavoriteAppsList.length).toBe(favoriteAppsList.length - 1);
+    expect(updatedFavoriteAppsList).not.toContain(appToDelete);
   });
 });
 
